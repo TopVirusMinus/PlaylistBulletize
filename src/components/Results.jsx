678b010fabac84ee120b9b6b9eca6404376029f4
TopVirusMinus <mwmma5000@gmail.com>
2023-02-07 19:38:13 +0200

    changed the color to white

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -15,37 +15,39 @@ const Results = ({ list }) => {
   const copyToast = () => toast.success("Copied To Clipboard!");
 
   return (
-    <div>
-      <label className="mr-2">
-        <input
-          type="radio"
-          name="listType"
-          value="bulleted"
-          checked={listType === "bulleted"}
-          onChange={() => setListType("bulleted")}
-          className="mr-2"
-        />
-        Bulleted
-      </label>
-      <label>
-        <input
-          type="radio"
-          name="listType"
-          value="numbered"
-          checked={listType === "numbered"}
-          onChange={() => setListType("numbered")}
-          className="mx-2"
-        />
-        Numbered
-      </label>
+    <div className="mx-auto w-fit mt-4">
+      <div className="flex flex-col ">
+        <label>
+          <input
+            type="radio"
+            name="listType"
+            value="bulleted"
+            checked={listType === "bulleted"}
+            onChange={() => setListType("bulleted")}
+            className="mr-2"
+          />
+          Bulleted
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="listType"
+            value="numbered"
+            checked={listType === "numbered"}
+            onChange={() => setListType("numbered")}
+            className="mr-2"
+          />
+          Numbered
+        </label>
+      </div>
 
-      <div class="read-only overflow-scroll w-fit max-w-80% mt-5 font-semibold block h-80 p-2.5 outline-none text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500">
+      <div class="read-only overflow-scroll w-fit max-w-80% mt-5 font-semibold block h-80 px-3 outline-none text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500">
         {listOfVideos.map((l) => (
           <div>{l}</div>
         ))}
       </div>
       <button
-        className="mt-2 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+        className="mt-2 mx-auto bg-white hover:bg-lime-500 hover:text-black text-gray-800 font-semibold py-2 px-4 border-2 border-lime-500 rounded shadow"
         onClick={() => {
           navigator.clipboard.writeText(copyList);
           copyToast();
