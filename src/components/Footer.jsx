f6d1a80fe718ce8e2b4078324b3d8d8de44f22e1
TopVirusMinus <mwmma5000@gmail.com>
2023-02-08 04:28:56 +0200

    fixed footer margin top

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="w-fit text-center h-fit mb-5">
+    <div className="w-fit text-center h-fit mb-5 mt-8">
       <p>
         Made With <span className="text-red-600">&#10084;</span> by Mustafa
         Marzouk
