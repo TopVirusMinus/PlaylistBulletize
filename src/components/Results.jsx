2e08fcc121c99223762695aa9fb8fb5703ed6f1a
TopVirusMinus <mwmma5000@gmail.com>
2023-02-08 04:04:42 +0200

    added scroll-x-auto

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -92,7 +92,7 @@ const Results = ({ list }) => {
           Reverse
         </label>
       </div>
-      <div className="read-only overflow-y-scroll  w-fit lg:max-w-80% sm:w-100% w-max-sm mt-5 font-semibold block h-80 px-3 outline-none text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500">
+      <div className="read-only overflow-y-scroll overflow-x-auto w-fit lg:max-w-80% sm:w-100% w-max-sm mt-5 font-semibold block h-80 px-3 outline-none text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500">
         {listOfVideos.map((l, i) => (
           <div key={i}>{l}</div>
         ))}
