ae7e9c93f52789fa255389f7ac9ddf27e8e60124
Mustafa Marzouk <mwmma5000@gmail.com>
2025-03-19 14:49:09 +0200

    feat: Added SpeedInsights

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -7,6 +8,7 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <SpeedInsights />
     <Analytics />
   </React.StrictMode>
 );
